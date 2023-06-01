0864396c6cf0728fc849b8f9fe0dfe841842f26e
carterce1997 <carterce1997@gmail.com>
2023-05-31 21:32:01 -0400

    sql script

---
+++ script.sql
@@ -0,0 +1,28 @@
+
+
+with 
+win_loss as (
+	select 
+		case
+			when "Result" = 'Win' then 1
+			else 0
+		end as win,
+		case
+			when "Result" = 'Loss' then 1
+			else 0
+		end as loss,
+		*
+	from christopher_carter
+)
+select 
+	sum("Boyd_Yards") as "Boyd Yards",
+	sum("Higgins_Yards") as "Higgins Yards",
+	sum("Chase_Yards") as "Chase Yards",
+	concat(sum(win), '-', sum(loss)) as "Win/Loss"
+from win_loss ;
+
+
+
+select 
+	*
+from christopher_carter cc ;
